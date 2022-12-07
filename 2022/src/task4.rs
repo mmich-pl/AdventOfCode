fc6d332df93e9aa7ceb0ba97795f06762320a4d8
mmich-pl <m.michal.parzych@gmail.com>
2022-12-07 20:11:33 +0100

    fix part 1, implement part 2 of task 4

--- 2022/src/task4.rs
+++ 2022/src/task4.rs
@@ -2,24 +2,27 @@ use std::str::FromStr;
 
 use crate::utils::lines_from_file;
 
-fn contains(elf_1:& [i8], elf_2: &[i8]) -> bool {
+fn contains(elf_1: &[i8], elf_2: &[i8]) -> bool {
     elf_1[0] <= elf_2[0] && elf_1[1] >= elf_2[1]
 }
 
-fn get_ranges(input: &[String], f: fn(& [i8], &[i8])->bool) -> i8 {
+fn contains_any(elf_1: &[i8], elf_2: &[i8]) -> bool {
+    !(elf_1[0] > elf_2[1] || elf_1[1] < elf_2[0])
+}
+fn get_ranges(input: &[String], f: fn(&[i8], &[i8]) -> bool) -> i16 {
     input.iter().map(|line| {
-    let ranges = line.split(',')
-                                    .map(|x: &str| x.split('-')
-                                                    .flat_map(i8::from_str)
-                                                    .collect())
-                                    .collect::<Vec<Vec<i8>>>();
-      (f(&ranges[0], &ranges[1]) ||f(&ranges[1], &ranges[0]) ) as i8 
+        let ranges = line.split(',')
+            .map(|x: &str| x.split('-')
+                .flat_map(i8::from_str)
+                .collect())
+            .collect::<Vec<Vec<i8>>>();
+        (f(&ranges[0], &ranges[1]) || f(&ranges[1], &ranges[0])) as i16
     }).sum()
 }
 
-pub fn main (){
-
+pub fn main() {
     let data = lines_from_file("src/data/task4.txt");
-    println!("{:?}",get_ranges(&data, contains));
+    println!("{:?}", get_ranges(&data, contains));
+    println!("{:?}", get_ranges(&data, contains_any));
 }
 
