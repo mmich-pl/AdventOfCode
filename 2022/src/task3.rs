b700aee90318af94e03b37ef7d7c6392ed1ff289
mmich-pl <m.michal.parzych@gmail.com>
2022-12-09 09:05:28 +0100

    task 3, part 2

--- 2022/src/task3.rs
+++ 2022/src/task3.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use itertools::Itertools;
 
 fn part_1(input: &str) -> u32{
      input.lines().filter_map(|line| {
@@ -13,10 +14,21 @@ fn part_1(input: &str) -> u32{
     }).sum()
 }
 
-
+fn part_2(input: &str) -> u32{
+    input.lines().map(|line| line.as_bytes())
+        .tuples()
+        .filter_map(|(sack1, sack2, sack3)| {
+        sack1.iter().find(|item| sack2.contains(item)&&sack3.contains(item))
+            .map(|item| match item {
+                b'a'..=b'z' => (item - b'a') +1,
+                _ => (item - b'A') + 26 +1
+            } as u32 )
+    }).sum()
+}
 
 
 pub fn main() {
     let input = fs::read_to_string("src/data/task3.txt").unwrap();
     println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
